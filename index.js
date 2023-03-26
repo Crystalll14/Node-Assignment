2ccc42148b5af3b6528f9a8b665dee137bd0699d
Crystal Nkemeh <104574407+Crystalll14@users.noreply.github.com>
2023-03-26 02:14:44 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello! My name is Crystal Nkemeh.");
+console.log("My preferred programming language is JavaScript.");;
+console.log("I hope to have built some interesting projects and gained practical experience in web development.");
+console.log("I also plan to keep up with the latest trends and technologies in the field, and maybe even contribute to some open-source projects.");
+
